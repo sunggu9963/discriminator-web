9ce093fb02a9a009858a6963d02807137e9d4786
seonggubag69@gmail.com <sunggu9963@gmail.com>
2025-06-29 20:45:26 +0900

    Second

--- src/main/java/com/example/demo/home/HomeController.java
+++ src/main/java/com/example/demo/home/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.demo.ConText.home;
+package com.example.demo.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
